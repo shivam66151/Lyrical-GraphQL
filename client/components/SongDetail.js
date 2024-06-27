2b7041513295c4be50d1ed71ff235e0229053b6c
shivam66151 <sharmashivam66151@gmail.com>
2024-06-27 16:58:13 +0530

    Songs Crud with React

---
+++ client/components/SongDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class SongDetail extends Component {
+    render() {
+        return (
+            <div>
+                <h3>SongDetail</h3>
+</div>
+        )
+    }
+}
+
+export default SongDetail
+
--- client/index.js
+++ client/index.js
@@ -1,8 +1,29 @@
+import './style/style.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { Router, Route, hashHistory, IndexRoute} from 'react-router'
+import { ApolloProvider } from 'react-apollo';
+import SongDetail from './components/SongDetail';
+
+import SongList from './components/SongList';
+import App from './components/App';
+import SongCreate from './components/SongCreate';
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path='/' component={App}>
+          <IndexRoute component={SongList} />
+          <Route path='songs/new' component={SongCreate} />
+          <Route path='/songs/:id' component={SongDetail} />
+        </Route> 
+   </Router>
+    </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
