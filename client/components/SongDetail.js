838c17cc2c08c48f785ca80d205f46f0d3228833
shivam66151 <sharmashivam66151@gmail.com>
2024-06-27 22:37:18 +0530

    lyrics mutations

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import fetchSong from '../queries/fetchSong'
+import {Link} from 'react-router'
+import LyricCreate from './LyricCreate'
 
 class SongDetail extends Component {
     render() {
+        const { song } = this.props.data
+        
+        if(!song) { return <div>Loading...</div>}
         return (
             <div>
+                <Link to='/'>Back</Link>
                 <h3>SongDetail</h3>
+                <LyricCreate songId={this.props.params.id} />
 </div>
         )
     }
 }
 
-export default SongDetail
+export default graphql(fetchSong, {
+    options: (props) => { return { variables:  { id: props.params.id}}}
+})(SongDetail)
 
