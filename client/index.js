b64363eb4bc6d3a1290be15ead740be63c0d11fe
shivam66151 <sharmashivam66151@gmail.com>
2024-06-28 22:57:52 +0530

    lyric list page

--- client/index.js
+++ client/index.js
@@ -1,14 +1,14 @@
-import './style/style.css'
+import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
-import { Router, Route, hashHistory, IndexRoute} from 'react-router'
 import { ApolloProvider } from 'react-apollo';
-import SongDetail from './components/SongDetail';
 
-import SongList from './components/SongList';
 import App from './components/App';
+import SongList from './components/SongList';
 import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
 
 const client = new ApolloClient({});
 
@@ -16,14 +16,14 @@ const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
-        <Route path='/' component={App}>
+        <Route path="/" component={App}>
           <IndexRoute component={SongList} />
-          <Route path='songs/new' component={SongCreate} />
-          <Route path='/songs/:id' component={SongDetail} />
-        </Route> 
-   </Router>
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
+        </Route>
+      </Router>
     </ApolloProvider>
-  )
+  );
 };
 
 ReactDOM.render(
